a70c9d823d183b74860ce15a1486cd0098bde617
Igor Chaika <kp580bm1@gmail.com>
2013-01-02 14:48:54 +0200

    Simple window.

---
+++ mainwindow.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <QtGui/QWidget>
+
+class MainWindow : public QWidget {
+	Q_OBJECT
+	Q_DISABLE_COPY(MainWindow);
+public:
+	MainWindow(QWidget * parent = 0);
+	virtual ~MainWindow() {}
+};
---
+++ pixed.pro
@@ -0,0 +1,4 @@
+OBJECTS_DIR = tmp
+MOC_DIR = tmp
+HEADERS = mainwindow.h
+SOURCES = main.cpp mainwindow.cpp
